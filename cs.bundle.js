aaeb15fede9f08da14d9fe4a5a17bde3d91ff477
Shane Hartman <shane@shanehartman.com>
2026-03-21 04:17:37 -0500

    Release v1.3.6

--- cs.bundle.js
+++ cs.bundle.js
@@ -28461,7 +28461,7 @@ import { hostname, homedir as homedir2 } from "os";
 import { readFileSync, existsSync } from "fs";
 import { join } from "path";
 import { homedir } from "os";
-var VERSION = "1.3.5";
+var VERSION = "1.3.6";
 var SCHEMA_VERSION = 1;
 var CONFIG_DIR = join(homedir(), ".config", "cs");
 var CONFIG_PATH = join(CONFIG_DIR, "config.json");
